3dadf4b51de6142625b7eb25c316f40c1d782065
Sahil <paalsahil04@gmail.com>
2025-07-10 21:14:15 +0530

    .env

--- util/location.js
+++ util/location.js
@@ -1,14 +1,17 @@
-const GOOGLE_API_KEY = 'AIzaSyCy4ULcsSC6aaOOggI0Etywil9i6ahQujQ';
+
+
+
 
 function getMapPreview(lat, lng) {
   const base = 'https://maps.googleapis.com/maps/api/staticmap';
+const api_key = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
   const params = [
     `center=${lat},${lng}`,
     `zoom=14`,
     `size=400x400`,
     `maptype=roadmap`,                                     // or 'terrain', 'satellite', etc.
     `markers=color:red%7Clabel:A%7C${lat},${lng}`,        // red pin labeled A
-    `key=${GOOGLE_API_KEY}`
+    `key=${api_key}`
   ];
   return `${base}?${params.join('&')}`;
 }
